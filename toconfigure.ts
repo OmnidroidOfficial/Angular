7c35aeddb773959dfd547e43779cd57cc644de16
OmnidroidOfficial <144283173+OmnidroidOfficial@users.noreply.github.com>
2023-11-08 15:59:23 +0100

    Create toconfigure.ts

---
+++ toconfigure.ts
@@ -0,0 +1,19 @@
+export class toconfigure {
+ 
+  public static country: string;
+  public static gender: string;
+  public static lastname: string;
+  public static notmaidenname: string;
+  public static terms1prefix: string;
+  public static terms1suffix: string;
+  public static ifmodulemarried: boolean;
+  public static ifmoduletitledprofession: boolean;
+  public static ifmoduledescent: boolean;
+  public static ifmodulehost: boolean;
+  public static echelonname: string;
+  public static firstname: string;
+  public static middlename: string;
+  public static email: string;
+  public static phone: string;
+  public static omnidroidid: string;
+}
